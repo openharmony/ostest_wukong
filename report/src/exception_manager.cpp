0fa4f9533eba1377a50af787d91a14ca2bcbb9b7
kangluming <kangluming@huawei.com>
2023-12-13 10:30:58 +0800

    revert Updated wukong to create separate file tags
    Signed-off-by: kangluming <kangluming@huawei.com>

--- report/src/exception_manager.cpp
+++ report/src/exception_manager.cpp
@@ -23,7 +23,6 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <vector>
-#include <policycoreutils.h>
 
 #include "csv_utils.h"
 #include "hisysevent_manager.h"
@@ -41,7 +40,6 @@ bool InitReportFolder()
 {
     DIR *rootDir = nullptr;
     if ((rootDir = opendir(DEFAULT_DIR.c_str())) == nullptr) {
-        Restorecon(DEFAULT_DIR.c_str());
         int ret = mkdir(DEFAULT_DIR.c_str(), S_IROTH | S_IRWXU | S_IRWXG);
         if (ret != 0) {
             ERROR_LOG_STR("failed to create dir: %s", DEFAULT_DIR.c_str());
